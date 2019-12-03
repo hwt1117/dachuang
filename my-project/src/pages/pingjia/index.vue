62acb406a0cb3513c95d54f5d9afecd522e86555
王天伟 <wang@wangtianweideMacBook-Pro.local>
2019-12-03 22:04:49 +0800

    提交

--- my-project/src/pages/pingjia/index.vue
+++ my-project/src/pages/pingjia/index.vue
@@ -1,16 +1,29 @@
 <template>
-  <div>
-      <textarea name="" id="" cols="30" rows="10"></textarea>
-      <button>提交</button>
-  </div>
+  <div class="main">
+    
+      <textarea name id cols="30" rows="10"></textarea>
+      <button style="height:50px;width:100%">提交</button>
+    </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
 <style>
-
+.main {
+  width: 100%;
+  height: 500px;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: center;
+  
+}
+textarea {
+  box-sizing: border-box;
+  width: 95%;
+  height: 300px;
+  border: 1px solid #5c89ff;
+}
 </style>
--- my-project/src/pages/remind/index.vue
+++ my-project/src/pages/remind/index.vue
@@ -1,17 +1,83 @@
 <template>
   <div>
-      <p>您的预约服务</p>
-      <p>预计还剩{{5+5}}分钟</p>
-      <button>确定</button>
+    <p>您的预约服务</p>
+    <p>预计时间还剩{{hour? hourString+':'+minuteString+':'+secondString : minuteString+':'+secondString}}</p>
+    <button>取消</button>
   </div>
 </template>
 
 <script>
 export default {
-
-}
+  data() {
+    return {
+      hour: '',
+        minute: '',
+        second: '',
+        promiseTimer: '',
+        remainTime:3 //设置时间
+      
+    };
+  },
+  computed: {
+      hourString () {
+        return this.formatNum(this.hour)
+      },
+      minuteString () {
+        return this.formatNum(this.minute)
+      },
+      secondString () {
+        return this.formatNum(this.second)
+      }
+    },
+  
+  methods: {
+    
+    countDowm () {
+        var self = this
+        clearInterval(this.promiseTimer)
+        this.promiseTimer = setInterval(function () {
+          if (self.hour === 0) {
+            if (self.minute !== 0 && self.second === 0) {
+              self.second = 59
+              self.minute -= 1
+            } else if (self.minute === 0 && self.second === 0) {
+              self.second = 0
+              self.$emit('countDowmEnd', true)
+              clearInterval(self.promiseTimer);
+              const url = "/pages/pingjia/main";
+              wx.navigateTo({url});
+            } else {
+              self.second -= 1
+            }
+          } else {
+            if (self.minute !== 0 && self.second === 0) {
+              self.second = 59
+              self.minute -= 1
+            } else if (self.minute === 0 && self.second === 0) {
+              self.hour -= 1
+              self.minute = 59
+              self.second = 59
+            } else {
+              self.second -= 1
+            }
+          }
+        }, 1000)
+      },
+      formatNum (num) {
+        return num < 10 ? '0' + num : '' + num
+      },
+  },
+  onLoad(options) {
+    if (this.remainTime > 0) {
+        this.hour = Math.floor((this.remainTime / 3600) % 24)
+        this.minute = Math.floor((this.remainTime / 60) % 60)
+        this.second = Math.floor(this.remainTime % 60)
+        this.countDowm()
+      }
+    
+  }
+};
 </script>
 
 <style>
-
 </style>
