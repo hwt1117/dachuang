d6eb8bbb6b3b6ce96712b3b8ceca802d5544babf
王天伟 <wang@wangtianweideMacBook-Pro.local>
2019-12-03 23:07:16 +0800

    ooo

--- my-project/src/pages/remind/index.vue
+++ my-project/src/pages/remind/index.vue
@@ -1,8 +1,10 @@
 <template>
   <div>
-    <p>您的预约服务</p>
-    <p>预计时间还剩{{hour? hourString+':'+minuteString+':'+secondString : minuteString+':'+secondString}}</p>
-    <button>取消</button>
+    <p class="title header">您的预约服务</p>
+    <p
+      class="time header"
+    >预计时间还剩:{{hour? hourString+':'+minuteString+':'+secondString : minuteString+':'+secondString}}</p>
+    <button @click="back" style="color:#5C89FF;width:60%;margin-top:130px;">取消</button>
   </div>
 </template>
 
@@ -10,74 +12,94 @@
 export default {
   data() {
     return {
-      hour: '',
-        minute: '',
-        second: '',
-        promiseTimer: '',
-        remainTime:3 //设置时间
-      
+      hour: "",
+      minute: "",
+      second: "",
+      promiseTimer: "",
+      remainTime: 3 //设置时间
     };
   },
   computed: {
-      hourString () {
-        return this.formatNum(this.hour)
-      },
-      minuteString () {
-        return this.formatNum(this.minute)
-      },
-      secondString () {
-        return this.formatNum(this.second)
-      }
+    hourString() {
+      return this.formatNum(this.hour);
     },
-  
+    minuteString() {
+      return this.formatNum(this.minute);
+    },
+    secondString() {
+      return this.formatNum(this.second);
+    }
+  },
+
   methods: {
-    
-    countDowm () {
-        var self = this
-        clearInterval(this.promiseTimer)
-        this.promiseTimer = setInterval(function () {
-          if (self.hour === 0) {
-            if (self.minute !== 0 && self.second === 0) {
-              self.second = 59
-              self.minute -= 1
-            } else if (self.minute === 0 && self.second === 0) {
-              self.second = 0
-              self.$emit('countDowmEnd', true)
-              clearInterval(self.promiseTimer);
-              const url = "/pages/pingjia/main";
-              wx.navigateTo({url});
-            } else {
-              self.second -= 1
-            }
+    back() {
+      wx.navigateBack({
+        delta: 1,
+        success: () => {
+          console.log("success");
+          clearInterval(this.promiseTimer);
+        },
+        fail: () => {
+          console.log("fail");
+        }
+      });
+    },
+
+    countDowm() {
+      var self = this;
+      clearInterval(this.promiseTimer);
+      this.promiseTimer = setInterval(function() {
+        if (self.hour === 0) {
+          if (self.minute !== 0 && self.second === 0) {
+            self.second = 59;
+            self.minute -= 1;
+          } else if (self.minute === 0 && self.second === 0) {
+            self.second = 0;
+            self.$emit("countDowmEnd", true);
+            clearInterval(self.promiseTimer);
+            const url = "/pages/pingjia/main";
+            wx.navigateTo({ url });
           } else {
-            if (self.minute !== 0 && self.second === 0) {
-              self.second = 59
-              self.minute -= 1
-            } else if (self.minute === 0 && self.second === 0) {
-              self.hour -= 1
-              self.minute = 59
-              self.second = 59
-            } else {
-              self.second -= 1
-            }
+            self.second -= 1;
           }
-        }, 1000)
-      },
-      formatNum (num) {
-        return num < 10 ? '0' + num : '' + num
-      },
+        } else {
+          if (self.minute !== 0 && self.second === 0) {
+            self.second = 59;
+            self.minute -= 1;
+          } else if (self.minute === 0 && self.second === 0) {
+            self.hour -= 1;
+            self.minute = 59;
+            self.second = 59;
+          } else {
+            self.second -= 1;
+          }
+        }
+      }, 1000);
+    },
+    formatNum(num) {
+      return num < 10 ? "0" + num : "" + num;
+    }
   },
   onLoad(options) {
     if (this.remainTime > 0) {
-        this.hour = Math.floor((this.remainTime / 3600) % 24)
-        this.minute = Math.floor((this.remainTime / 60) % 60)
-        this.second = Math.floor(this.remainTime % 60)
-        this.countDowm()
-      }
-    
+      this.hour = Math.floor((this.remainTime / 3600) % 24);
+      this.minute = Math.floor((this.remainTime / 60) % 60);
+      this.second = Math.floor(this.remainTime % 60);
+      this.countDowm();
+    }
   }
 };
 </script>
 
 <style>
+.header {
+  width: 100%;
+  font-size: 20px;
+  /* margin-left: 10%;  */
+  margin-bottom: 5%;
+  text-align: center;
+}
+.title {
+  margin-top: 100px;
+}
 </style>
--- my-project/src/pages/verbOnline/index.vue
+++ my-project/src/pages/verbOnline/index.vue
@@ -1,9 +1,9 @@
 <template>
   <div>
-    <p>业务类型：{{Info.yewu}}</p>
-    <p>当前等候人数：{{Info.people}}</p>
-    <a>与银行联系{{Info.chat}}</a>
-    <button @click="godetails(3)">预约</button>
+    <p class="title header">业务类型：{{Info.yewu}}</p>
+    <p class="people header">当前等候人数：{{Info.people}}</p>
+    <a class="getconnect header">与银行联系{{Info.chat}}</a>
+    <button style="color:#5C89FF;width:60%;margin-top:130px;" @click="godetails(3)">预约</button>
   </div>
 </template>
 
@@ -26,6 +26,14 @@ export default {
   }
 };
 </script>
-
 <style>
+.header{
+  font-size: 20px;
+  /* margin-left: 10%;  */
+  margin-bottom: 5%;
+  text-align: center;
+}
+.title{
+  margin-top: 100px;
+}
 </style>
